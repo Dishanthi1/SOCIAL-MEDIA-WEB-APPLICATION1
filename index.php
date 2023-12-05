1dd0bfe86625c7bc6bd6e32dee5a2dc5984e51ea
Dishanthi1 <147810788+Dishanthi1@users.noreply.github.com>
2023-12-05 20:49:16 +0530

    post option

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <?php endif;?>
 <div class="card card-outline rounded-0 card-navy">
 	<div class="card-header">
-		<h3 class="card-title">List of members</h3>
+		<h3 class="card-title">List of Posts</h3>
 	</div>
 	<div class="card-body">
         <div class="container-fluid">
@@ -14,37 +14,43 @@
 				<colgroup>
 					<col width="5%">
 					<col width="15%">
-					<col width="45%">
 					<col width="20%">
-					<col width="15%">
+					<col width="30%">
+					<col width="10%">
+					<col width="10%">
+					<col width="10%">
 				</colgroup>
 				<thead>
 					<tr>
 						<th>#</th>
 						<th>Date Created</th>
-						<th>Name</th>
-						<th>Email</th>
+						<th>Member</th>
+						<th>Caption</th>
+						<th>Likes</th>
+						<th>Comments</th>
 						<th>Action</th>
 					</tr>
 				</thead>
 				<tbody>
 					<?php 
 					$i = 1;
-						$qry = $conn->query("SELECT *, CONCAT(lastname, ' ', firstname, ' ', coalesce(middlename,'')) as `name` from `member_list` order by `name` asc ");
+						$qry = $conn->query("SELECT p.*, concat(m.firstname, ' ', coalesce(concat(m.middlename,' '),''),m.lastname) as `name`, m.avatar, COALESCE((SELECT count(member_id) FROM `like_list` where post_id = p.id),0) as `likes`, COALESCE((SELECT count(member_id) FROM `comment_list` where post_id = p.id),0) as `comments` FROM post_list p inner join `member_list` m on p.member_id = m.id  order by unix_timestamp(p.date_updated) desc");
 						while($row = $qry->fetch_assoc()):
 					?>
 						<tr>
 							<td class="text-center"><?php echo $i++; ?></td>
 							<td><?php echo date("Y-m-d H:i",strtotime($row['date_created'])) ?></td>
 							<td><?php echo $row['name'] ?></td>
-							<td><?php echo $row['email'] ?></td>
+							<td class=""><p class="m-0 truncate-1"><?= $row['caption'] ?></p></td>
+							<td class="text-right"><?php echo format_num($row['likes']) ?></td>
+							<td class="text-right"><?php echo format_num($row['comments']) ?></td>
 							<td align="center">
 								 <button type="button" class="btn btn-flat p-1 btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
 				                  		Action
 				                    <span class="sr-only">Toggle Dropdown</span>
 				                  </button>
 				                  <div class="dropdown-menu" role="menu">
-				                    <a class="dropdown-item view_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-eye text-dark"></span> View</a>
+				                    <a class="dropdown-item view_data" href="./?page=posts/view_post&id=<?php echo $row['id'] ?>"><span class="fa fa-eye text-dark"></span> View</a>
 				                    <div class="dropdown-divider"></div>
 				                    <a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> Delete</a>
 				                  </div>
@@ -58,24 +64,21 @@
 </div>
 <script>
 	$(document).ready(function(){
-		$('.view_data').click(function(){
-			uni_modal('<i class="fa fa-id-card"></i> Member Details', 'members/view_member.php?id='+$(this).attr('data-id'))
-		})
 		$('.delete_data').click(function(){
-			_conf("Are you sure to delete this member permanently?","delete_member",[$(this).attr('data-id')])
+			_conf("Are you sure to delete this post permanently?","delete_post",[$(this).attr('data-id')])
 		})
 		$('.table').dataTable({
 			columnDefs: [
-					{ orderable: false, targets: [5] }
+					{ orderable: false, targets: [4] }
 			],
 			order:[0,'asc']
 		});
 		$('.dataTable td,.dataTable th').addClass('py-1 px-2 align-middle')
 	})
-	function delete_member($id){
+	function delete_post($id){
 		start_loader();
 		$.ajax({
-			url:_base_url_+"classes/User.php?f=delete_member",
+			url:_base_url_+"classes/Master.php?f=delete_post",
 			method:"POST",
 			data:{id: $id},
 			dataType:"json",
